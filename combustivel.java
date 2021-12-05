afb864a7b94907a6f83d33c2c67fb27c56ac535c
RodrigoSoaresNascimento <rodrigosnascimento1@gmail.com>
2021-12-05 16:57:33 -0300

    Adicionei o titulo

--- combustivel.java
+++ combustivel.java
@@ -3,6 +3,8 @@ public class combustivel {
     public static void main(String[]agrs){
         Scanner entrada = new Scanner(System.in);
 
+        //Um posto de combustíveis deseja determinar qual de seus produtos tem a preferência de seus clientes. Escreva um algoritmo para ler o tipo de combustível abastecido (codificado da seguinte forma: 1.Álcool 2.Gasolina 3.Diesel 4.Fim). Caso o usuário informe um código inválido (fora da faixa de 1 a 4) deve ser solicitado um novo código (até que seja válido). O programa será encerrado quando o código informado for o número 4, devendo então mostrar a mensagem "MUITO OBRIGADO", bem como as quantidades de cada combustível.//
+
         int alcool = 0, gasolina = 0, diesel = 0;
         int produto;
         System.out.println("Informe um codigo (1, 2, 3) ou 4 para parar: ");
